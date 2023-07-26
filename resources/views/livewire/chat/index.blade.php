a488a284425ebeb1f7dbb7cc7dd67bbc29874cb7
breemark <britomaximiliano83@gmail.com>
2023-07-26 11:52:51 +0700

    Chat Index component

--- resources/views/livewire/chat/index.blade.php
+++ resources/views/livewire/chat/index.blade.php
@@ -1,4 +1,15 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
-    Index
+{{-- Close your eyes. Count to one. That is how long forever feels. --}}
+
+<div class="fixed h-full flex bg-white border lg:shadow-sm overflow-hidden inset-0 lg:top-16 lg:inset-x-2 m-auto lg:h-[90%] rounded-t-lg">
+    
+    <div class="relative w-full md:w-[320px] xl:w-[400px] overflow-y-auto shrink-0 h-full border">
+        List
+    </div>
+
+    <div class="hidden md:grid w-full border-l h-full relative overflow-y-auto" style="contain:content">
+        
+        <div class="m-auto text-center justify-center flex flex-col gap-3">
+            <h4 class="font-medium text-lg"> Choose a conversation</h4>
+        </div>
+    </div>
 </div>
